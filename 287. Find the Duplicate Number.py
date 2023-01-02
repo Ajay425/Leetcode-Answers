c9493a0a70a8ff8d08e9b5327cfa06ae8ded00e8
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2023-01-01 19:01:55 -0500

    Update 287. Find the Duplicate Number.py
    added another solution

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -1,3 +1,4 @@
+#Solution 1
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
@@ -5,3 +6,13 @@ class Solution:
         for key,values in counters.items():
             if values > 1:
                 return key
+
+           
+        
+#Solution 2        
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+       for i in nums:
+            nums[abs(i)-1] *= -1
+            if nums[abs(i)-1] > 0: return abs(i)
